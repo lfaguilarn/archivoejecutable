19b7949707a09b3236150db043e45837ce058cb2
lfaguilarn <luisfernandoaguilar56@gmail.com>
2023-12-28 14:32:10 -0500

    actualizacion de mensaje
    actualizacion de mensaje

--- src/main/java/prueba/archivoejecutable/ventana.java
+++ src/main/java/prueba/archivoejecutable/ventana.java
@@ -63,7 +63,7 @@ public class ventana extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       JOptionPane.showMessageDialog(null, "Prueba 1", "prueba", 3);
+       JOptionPane.showMessageDialog(null, "Prueba 3", "Información", 3);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     
