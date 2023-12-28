aae3b92a53a9502864be95fa5919a450299fd198
lfaguilarn <luisfernandoaguilar56@gmail.com>
2023-12-28 13:56:06 -0500

    commit java project
    commit java project

---
+++ src/main/java/prueba/archivoejecutable/principal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba.archivoejecutable;
+
+/**
+ *
+ * @author Luis Fernando
+ */
+public class principal {
+    public static void main(String [] args){
+        ventana v = new ventana();
+        v.setVisible(true);
+    }
+}
